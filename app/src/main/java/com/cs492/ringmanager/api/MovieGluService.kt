0606de9a9b5e80b524d8c676cb35dc17c8d0ff9f
Jared-Zielinski <92007923+Jared-Zielinski@users.noreply.github.com>
2022-03-15 14:51:36 -0700

    Just a little cleanup of MovieGluService.kt

--- app/src/main/java/com/cs492/ringmanager/api/MovieGluService.kt
+++ app/src/main/java/com/cs492/ringmanager/api/MovieGluService.kt
@@ -1,28 +1,30 @@
 package com.cs492.ringmanager.api
 
+import android.location.Location
 import com.cs492.ringmanager.data.LocationData
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 import retrofit2.http.Headers
 
 //https://developer.movieglu.com/v2/api-index/cinemasNearby/
-//https://api-gate2.movieglu.com/cinemasNearby/?n=5
+//https://api-gate2.movieglu.com/cinemasNearby/?n=25
 interface MovieGluService {
-    @Headers({
-        "client: OREG ",
-        "x-api-key: x18jFgFa0n7sxB0k2QxR15YlOAeo5OUo8cJyhMnj ",
-        "authorization: Basic T1JFR19YWDpCWm9ub1d5UDJFMUw= ",
-        "territory: XX ",
-        "api-version: v200 "})
+    @Headers(
+        "client: OREG",
+        "x-api-key: x18jFgFa0n7sxB0k2QxR15YlOAeo5OUo8cJyhMnj",
+        "authorization: Basic T1JFR19YWDpCWm9ub1d5UDJFMUw=",
+        "territory: XX",
+        "api-version: v200")
     @GET("cinemasNearby")
     suspend fun getCinemasNearby(
         @Header("Geolocation") geolocation: String,
-        @Header("Device-datetime") device-datetime: String,
-        @Query("n") maxResults: Int = 25):ListOf<LocationWithRing>
+        @Header("Device-datetime") datetime: String,
+        @Query("n") maxResults: Int = 25): List<LocationData>
 
     companion object{
         private const val BASE_URL = "https://api-gate2.movieglu.com"
--- app/src/main/java/com/cs492/ringmanager/data/LocationData.kt
+++ app/src/main/java/com/cs492/ringmanager/data/LocationData.kt
@@ -1,12 +1,13 @@
 package com.cs492.ringmanager.data
 
 import androidx.room.Entity
+import com.squareup.moshi.Json
 import java.io.Serializable
 
 @Entity(primaryKeys = ["latitude", "longitude"])
 data class LocationData(
     @Json(name = "lat") val latitude: Double,
     @Json(name = "lng") val longitude: Double,
-    val radius: Float = 75,
+    val radius: Float = 75F,
     val name: String
 ) : Serializable
